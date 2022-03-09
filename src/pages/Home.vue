09006ab2b242fcedcc8fd03b38fdf9659f66a10e
WaydeLagrandeur <waydelagrandeur@gmail.com>
2022-03-08 20:27:39 -0600

    Added Test Shrek

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,23 @@
 <template>
     <div>
-        <h1>Hello World!</h1>
-        <Carousel />
+        <img id='shrek' src='https://cdn.vox-cdn.com/thumbor/qCfHPH_9Mw78vivDlVDMu7xYc78=/715x248:1689x721/920x613/filters:focal(972x299:1278x605):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69305239/shrek4_disneyscreencaps.com_675.0.jpg' />
+        <!--<Carousel /> -->
     </div>
 </template>
 
 <script>
-import Carousel from '../components/Carousel.vue'
+//import Carousel from '../components/Carousel.vue'
 export default {
     name: 'Home',
-    components: {
-        Carousel,
-    }
+    // components: {
+    //     Carousel,
+    // }
 }
 </script>
+<style scoped>
+    #shrek {
+        display: block;
+        margin: auto;
+        padding: 5vw;
+    }
+</style>
