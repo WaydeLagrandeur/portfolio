a02cfebcaf9984c30c130ca72234205fb19361f4
WaydeLagrandeur <waydelagrandeur@gmail.com>
2022-03-08 20:47:02 -0600

    Testing on Pi, added h1 tag

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <img id='shrek' src='https://cdn.vox-cdn.com/thumbor/qCfHPH_9Mw78vivDlVDMu7xYc78=/715x248:1689x721/920x613/filters:focal(972x299:1278x605):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69305239/shrek4_disneyscreencaps.com_675.0.jpg' />
-        <!--<Carousel /> -->
+        <h1 class="welcome">HI BEEPIS</h1>
     </div>
 </template>
 
@@ -18,6 +18,12 @@ export default {
     #shrek {
         display: block;
         margin: auto;
-        padding: 5vw;
+        padding: 2vw;
+    }
+    .welcome {
+	display: block;
+	margin: auto;
+	padding: 2vw;
+	text-align: center;
     }
 </style>
